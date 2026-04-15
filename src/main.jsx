6036d70345c5c5a8e6af9053e1d27fd7547ba217
VenturiVen <gingeroshea443@gmail.com>
2026-04-15 21:19:09 +0100

    Error boundary component and loading fallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import { RouterProvider } from 'react-router-dom'
 import router from './app/router.jsx'
+import AppErrorBoundary from "./ui/components/AppErrorBoundary/index.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <AppErrorBoundary>
+        <Provider store={store}>
+          <Suspense fallback={<div>Loading app...</div>}>
+            <RouterProvider router={router} />
+          </Suspense>
+        </Provider>
+    </AppErrorBoundary>
   </StrictMode>
 )
